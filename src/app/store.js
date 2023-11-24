05308feb7240e9316c8a27439fd5e87c911f8740
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-11-24 22:58:36 +0100

    Removes counterSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import boardReducer from '../features/boardSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     board: boardReducer,
   },
 })
