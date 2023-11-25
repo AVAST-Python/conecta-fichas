13c24cc343d20b4f807515a032b989fc6b8e2deb
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-11-25 07:28:15 +0100

    Changes layout to responsive grid

--- src/Board.js
+++ src/Board.js
@@ -8,35 +8,36 @@ import Square from './Square'
 function Board() {
 
   const dispatch = useDispatch()
-  const {fixed, current} = useSelector(state => state.board)
+  const { fixed, current } = useSelector(state => state.board)
 
   function onPressed(row, col) {
     dispatch(leftClick([row, col]))
   }
   function onRightPressed(row, col) {
-    dispatch(rightClick([row,col]));
+    dispatch(rightClick([row, col]));
   }
 
   return (
 
-    <div className="Board">
-      <table>
-        <tbody>
-          {current.map((row, i) => (
-            <tr key={i}>
-              {row.map((cell, j) => {
-                const isFixed = fixed.some(([row, col]) => row === i && col === j)
-                return (
-                  <Square key={j} row={i} col={j} value={cell} fixed={isFixed}
-                    onPressed={onPressed}
-                    onRightPressed={onRightPressed}
-                  />
-                )
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="board">
+      {current.map((row, i) => (
+        row.map((cell, j) => {
+          const isFixed = fixed.some(([row, col]) => row === i && col === j)
+
+          const isLastRow = i === current.length - 1
+          const isLastCol = j === row.length - 1
+
+          return (
+            <Square key={`${i}-${j}`}
+              row={i} col={j} value={cell}
+              fixed={isFixed}
+              parentClasses={`${isLastRow ? "last-row" : ""} ${isLastCol ? "last-col" : ""}`}
+              onPressed={onPressed}
+              onRightPressed={onRightPressed}
+            />
+          )
+        })
+      ))}
     </div>
   )
 
--- src/Square.js
+++ src/Square.js
@@ -6,18 +6,19 @@ import { EMPTY, WHITE } from './features/boardSlice'
 function Square(props) {
   const {row, col, value, fixed } = props
   const {onPressed, onRightPressed} = props
+  const {parentClasses} = props
 
   const squareClass=value === EMPTY ? "empty" : value === WHITE ? "white" : "black"
 
   return (
-    <td
-      className={`${fixed ? "fixed" : ""}`}
+    <div
+      className={`cell ${fixed ? "fixed" : ""} ${parentClasses}`}
       onClick={() => onPressed(row, col)}
       onContextMenu={(e) => {e.preventDefault(); onRightPressed(row, col)}}
     >
       { value !== EMPTY &&
       <div className={`piece ${squareClass}`} ></div>}
-    </td>
+    </div>
   )
 
 }
